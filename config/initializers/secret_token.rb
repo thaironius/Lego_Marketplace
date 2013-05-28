6b7d95750ca6a86370be3a5f4fcdc08024059818
Thaironius <thaironius@gmail.com>
2013-05-28 15:04:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LegoMarketplace::Application.config.secret_token = 'c70a43cb8eb844786ce294877f547c583d26e3a5744163b98413fa676f0caf091891b6de0530e4aa59f0c85386fdc4c819b4d6b34964b6d2086b553e00ea7999'
