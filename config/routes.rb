6502175ba5981ceb29e758c5e07256fff470cb5f
Thaironius <thaironius@gmail.com>
2013-05-28 17:00:00 -0700

    Added root :to => 'pages#main' in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 LegoMarketplace::Application.routes.draw do
   resources :homes
 
+root :to => 'pages#main'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
